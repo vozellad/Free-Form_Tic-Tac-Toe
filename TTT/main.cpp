554a49fdf2270a1235ae594805c997072fed8d0c
Demetrios Vozella <57552104+vozellad@users.noreply.github.com>
2022-05-11 20:22:55 -0400

    Added project folder.

---
+++ TTT/main.cpp
@@ -0,0 +1,15 @@
+// TODO:
+//
+// Create animations for adding/removing elements and changing screens.
+
+#include "mainwindow.h"
+
+#include <QApplication>
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+    MainWindow w;
+    w.show();
+    return a.exec();
+}
