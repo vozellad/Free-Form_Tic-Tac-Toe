9b6a3fc2e7442c278d8715b49351bc400f7ca3a2
Demetrios Vozella <57552104+vozellad@users.noreply.github.com>
2023-05-17 21:01:26 -0400

    making progress on dynamic player amount adjusting

--- gamesetupwindow.cpp
+++ gamesetupwindow.cpp
@@ -22,3 +22,51 @@ void GameSetupWindow::on_pushButton_back_clicked()
 
     this->hide();
 }
+
+void GameSetupWindow::on_pushButton_startGame_clicked()
+{
+
+}
+
+void GameSetupWindow::on_spinBox_playerAmt_valueChanged(int newPlayerAmt)
+{
+    static int playerAmt = ui->spinBox_playerAmt->value();
+    // remove spacer
+
+    if (newPlayerAmt < playerAmt)
+        for (int i = 0; i < playerAmt - newPlayerAmt; i++)
+            removePlayer();
+    else
+        for (int i = 0; i < newPlayerAmt - playerAmt; i++)
+            addPlayer();
+
+    // add spacer
+    playerAmt = newPlayerAmt;
+}
+
+void GameSetupWindow::addPlayer()
+{
+    QGridLayout* playerSetup = ui->gridLayout_playerSetup;
+
+    playerSetup->addWidget(new QLineEdit("temp txt"));
+    playerSetup->addWidget(new QLabel("..."));
+    playerSetup->addWidget(new QToolButton());
+}
+
+void GameSetupWindow::removePlayer()
+{
+    QGridLayout* playerSetup = ui->gridLayout_playerSetup;
+
+    for (int i = 0; i < 3; i++)
+    {
+        auto lastItem = playerSetup->takeAt(playerSetup->count() - 1);
+        delete lastItem->widget();
+        delete lastItem;
+    }
+
+    int row = int(playerSetup->count() / 3);
+    if (row % 3 == 0) {
+        playerSetup->setRowMinimumHeight(row, 0);
+        playerSetup->setRowStretch(row, 0);
+    }
+}
--- gamesetupwindow.h
+++ gamesetupwindow.h
@@ -19,8 +19,17 @@ private slots:
     // Go to previous window
     void on_pushButton_back_clicked();
 
+    void on_pushButton_startGame_clicked();
+
+    void on_spinBox_playerAmt_valueChanged(int newPlayerAmt);
+
 private:
     Ui::GameSetupWindow *ui;
+    //QGridLayout* playerSetup = ui->gridLayout_playerSetup;
+
+    void addPlayer();
+
+    void removePlayer();
 };
 
 #endif // GAMESETUPWINDOW_H
