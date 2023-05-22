3e15e9348f09c9099ab91829fed28ea3ae759cde
Demetrios Vozella <57552104+vozellad@users.noreply.github.com>
2023-05-22 16:07:41 -0400

    user can now add boards
    although the spinboxes have inner padding and I don't know why

--- gamesetupwindow.cpp
+++ gamesetupwindow.cpp
@@ -3,16 +3,31 @@
 #include "startscreenwindow.h"
 #include "playersymboldialog.h"
 
+// TODO: split file into gamesetup, playersetup, and boardsetup
+
 GameSetupWindow::GameSetupWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::GameSetupWindow)
 {
     ui->setupUi(this);
 
+    // hides element that helps center the title
     ui->pushButton_backBalancer->setVisible(false);
+
+    // make - + buttons square
+    QToolButton* buttonArray[4] = {
+        ui->toolButton_addPlayer,
+        ui->toolButton_removePlayer,
+        ui->toolButton_addBoard,
+        ui->toolButton_removeBoard
+    };
+    for (QToolButton* b : buttonArray)
+        b->setMinimumWidth(b->height());
 
     players = ui->gridLayout_players;
     addInitialPlayers();
+
+    boards = ui->verticalLayout_boards;
 }
 
 GameSetupWindow::~GameSetupWindow()
@@ -88,7 +103,7 @@ void GameSetupWindow::setPlayerAmt(int newPlayerAmt)
     ui->label_playerAmtDisplay->setText(QString::number(newPlayerAmt));
 }
 
-void GameSetupWindow::setButtonStates() // TODO: find better function name
+void GameSetupWindow::setButtonStates() // TODO: find better function name  // TODO: make for boards
 {
     ui->toolButton_addPlayer->setEnabled(true);
     ui->toolButton_removePlayer->setEnabled(true);
@@ -97,6 +112,16 @@ void GameSetupWindow::setButtonStates() // TODO: find better function name
         ui->toolButton_removePlayer->setEnabled(false);
     else if (99 <= getPlayerAmt())
         ui->toolButton_addPlayer->setEnabled(false);
+
+    //
+
+    ui->toolButton_addBoard->setEnabled(true);
+    ui->toolButton_removeBoard->setEnabled(true);
+
+    if (getBoardAmt() <= 1)
+        ui->toolButton_removeBoard->setEnabled(false);
+    else if (99 <= getBoardAmt())
+        ui->toolButton_addBoard->setEnabled(false);
 }
 
 void GameSetupWindow::deleteLastGridItem(QGridLayout* l)
@@ -130,4 +155,70 @@ void GameSetupWindow::addInitialPlayers()
     newSymbol->setText("O");
     on_playerSymbol_clicked(newSymbol, newNameStr);
     players->addWidget(newSymbol, 1, 1);
+}
+
+void GameSetupWindow::on_toolButton_addBoard_clicked()
+{
+    // increment board amount
+    setBoardAmt(getBoardAmt() + 1);
+
+    QFrame* hLine = new QFrame;
+    hLine->setFrameShape(QFrame::HLine);
+    hLine->setFrameShadow(QFrame::Sunken);
+    boards->addWidget(hLine);
+
+    QGridLayout* newBoard = new QGridLayout();
+
+    QString numStr = tr("<b>Board %1<b>").arg(getBoardAmt());
+    QLabel* num = new QLabel(numStr);
+    newBoard->addWidget(num);
+
+    QSpacerItem* spacerRight = new QSpacerItem(5,
+                                               5,
+                                               QSizePolicy::Expanding,
+                                               QSizePolicy::Minimum);
+    newBoard->addItem(spacerRight, 0, 4);
+
+    newBoard->addWidget(new QLabel("Size of board: "), 1, 0);
+
+    QSpinBox* sizeRow = new QSpinBox();
+    sizeRow->setValue(3);
+    sizeRow->setMaximumWidth(55);  // TODO: why different
+    sizeRow->setMinimum(1);
+    newBoard->addWidget(sizeRow);
+
+    newBoard->addWidget(new QLabel("x"));
+
+    QSpinBox* sizeCol = new QSpinBox();
+    sizeCol->setValue(3);
+    sizeCol->setMaximumWidth(55);
+    sizeCol->setMinimum(1);
+    newBoard->addWidget(sizeCol);
+
+    newBoard->addWidget(new QLabel("In a row to win: "), 2, 0);
+
+    QSpinBox* win = new QSpinBox();
+    win->setValue(3);
+    win->setMaximumWidth(55);
+    win->setMinimum(1);
+    newBoard->addWidget(win);
+
+    boards->addLayout(newBoard);
+
+    setButtonStates();
+}
+
+void GameSetupWindow::on_toolButton_removeBoard_clicked()
+{
+
+}
+
+int GameSetupWindow::getBoardAmt()
+{
+    return ui->label_boardAmtDisplay->text().toInt();
+}
+
+void GameSetupWindow::setBoardAmt(int newBoardAmt)
+{
+    ui->label_boardAmtDisplay->setText(QString::number(newBoardAmt));
 }
