a56a207862688b3fd191e3879bc11b067e7421f0
Demetrios Vozella <57552104+vozellad@users.noreply.github.com>
2023-05-22 20:44:14 -0400

    Commented gamesetupwindow.h

--- gamesetupwindow.h
+++ gamesetupwindow.h
@@ -29,39 +29,62 @@ private slots:
     // Go to next window
     void on_pushButton_startGame_clicked();
 
+    // Add new player row to list of players
     void on_toolButton_addPlayer_clicked();
 
+    // Remove last player row from list of players
     void on_toolButton_removePlayer_clicked();
 
+    // Prompts user for new symbol to replace clicked symbol
     void on_playerSymbol_clicked(ClickableLabel* symbol, const QString name);
 
+    // Add new board to list of boards
     void on_toolButton_addBoard_clicked();
 
+    // Remove last board from list of boards
     void on_toolButton_removeBoard_clicked();
 
 private:
     Ui::GameSetupWindow *ui;
 
+    // List of players
     QGridLayout* players;
 
+    // List of boards
     QVBoxLayout* boards;
 
+    // Get current amount of players
     int getPlayerAmt() const;
 
+    // Set current amount of players
     void setPlayerAmt(const int newPlayerAmt);
 
+    // Keeps player amount within range (1-99)
+    // If not in range, turn off appropriate player amount modifier button -/+
+    // to prevent user from going outside range
     void setAmtModBtnStates_players();
 
+    // Delete last item in given layout
     void deleteLastItem(QLayout* l);
 
+    // Deleting from a grid doesn't reduce the grid size appropriately.
+    // This makes sure it is.
     void reAdjustGridSize(QGridLayout* l);
 
+    // Removing players is done by removing recent elements.
+    // This makes sure the elements of the player are ordered correctly
+    // to be correctly removed.
     void addInitialPlayers();
 
+    // Get current amount of boards
     int getBoardAmt() const;
 
+    // Set current amount of boards
     void setBoardAmt(int newBoardAmt);
 
+    // Keeps board amount within range (1-99)
+    // If not in range, turn off appropriate board amount modifier button -/+
+    // to prevent user from going outside range
     void setAmtModBtnStates_boards();
 };
 
